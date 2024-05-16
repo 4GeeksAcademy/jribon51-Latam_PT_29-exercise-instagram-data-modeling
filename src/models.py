d69b52fd589a284c637425295a158bf400fb059b
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-05-16 04:33:33 +0000

    se completa proyecto

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import Enum
 
 Base = declarative_base()
 
@@ -24,6 +25,45 @@ class Address(Base):
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+
+class User(Base):
+    __tablename__='user'
+    id=Column(Integer,primary_key=True)
+    username=Column(String(50))
+    firstname=Column(String(50))
+    lastname=Column(String(50))
+    email=Column(String(50))
+
+class Post(Base):
+    __tablename__='post'
+    id=Column(Integer,primary_key=True)
+    user_id=Column(Integer,ForeignKey('user.id'))
+    user=relationship(User)
+
+class Comment(Base):
+    __tablename__='comment'
+    id=Column(Integer,primary_key=True)
+    comment_text=Column(String(50))
+    author_id=Column(Integer,ForeignKey('user.id'))
+    user=relationship(User)
+    post_id=Column(Integer,ForeignKey('post.id'))
+    post=relationship(Post)
+
+class Media(Base):
+    __tablename__='media'
+    id=Column(Integer,primary_key=True)
+    type=Column(Enum('image', 'video', name='media_type_enum'))
+    url=Column(String(50))
+    post_id=Column(Integer,ForeignKey('post.id'))
+    post=relationship(Post)
+
+class Follower(Base):
+    __tablename__='follower'
+    id=Column(Integer,primary_key=True)
+    user_from_id=Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+    user_to_id=Column(Integer,ForeignKey('user.id'))
+    user_id=relationship('User')
 
     def to_dict(self):
         return {}
